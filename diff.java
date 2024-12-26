8007b2aa09daa249a94ed0bae1831cbcb8462fe8
sritha <srithat230@gmail.com>
2024-12-26 18:41:58 +0530

    all commited

---
+++ diff.java
@@ -0,0 +1,7 @@
+public class diff {
+    public static void main(String[] args) {
+        int a=23;
+        int b=13;
+        System.out.println(a-b);
+    }
+}
---
+++ mul.java
@@ -0,0 +1,7 @@
+public class mul {
+    public static void main(String[] args) {
+        int a=23;
+        int b=13;
+        System.out.println(a*b);
+    }
+}
